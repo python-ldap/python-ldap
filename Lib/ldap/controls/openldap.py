086e68976bf420b9e93a63c5c1986543b219844f
stroeder <stroeder>
2015-06-22 11:51:07 +0000

    New mix-in class ldap.controls.openldap.SearchNoOpMixIn adds convience method noop_search_st() to LDAPObject class

--- Lib/ldap/controls/openldap.py
+++ Lib/ldap/controls/openldap.py
@@ -3,7 +3,7 @@ ldap.controls.openldap - classes for OpenLDAP-specific controls
 
 See http://www.python-ldap.org/ for project details.
 
-$Id: openldap.py,v 1.1 2013/07/05 16:57:25 stroeder Exp $
+$Id: openldap.py,v 1.2 2015/06/22 11:51:07 stroeder Exp $
 """
 
 import ldap.controls
@@ -42,3 +42,36 @@ class SearchNoOpControl(ValueLessRequestControl,ResponseControl):
 
 ldap.controls.KNOWN_RESPONSE_CONTROLS[SearchNoOpControl.controlType] = SearchNoOpControl
 
+
+class SearchNoOpMixIn:
+  """
+  Mix-in class to be used with class LDAPObject and friends.
+  
+  It adds a convenience method noop_search_st() to LDAPObject
+  for easily using the no-op search control.
+  """
+  
+  def noop_search_st(self,base,scope=ldap.SCOPE_SUBTREE,filterstr='(objectClass=*)',timeout=-1):
+    try:
+      msg_id = self.search_ext(
+        base,
+        scope,
+        filterstr=filterstr,
+        attrlist=['1.1'],
+        timeout=timeout,
+        serverctrls=[SearchNoOpControl(criticality=True)],
+      )
+      _,_,_,search_response_ctrls = self.result3(msg_id,all=1,timeout=timeout)
+    except LDAPLimitErrors,e:
+      self.abandon(msg_id)
+      raise e
+    else:
+      noop_srch_ctrl = [
+        c
+        for c in search_response_ctrls
+        if c.controlType==SearchNoOpControl.controlType
+      ]
+      if noop_srch_ctrl:
+        return noop_srch_ctrl[0].numSearchResults,noop_srch_ctrl[0].numSearchContinuations
+      else:
+        return (None,None)
