3fb192f92740a7cf60a28d823291760688a9ed69
stroeder <stroeder>
2016-07-24 15:40:17 +0000

    tests for ldap.dn.is_dn()

---
+++ Tests/t_ldap_dn.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Automatic tests for module ldap.functions
+"""
+
+# from Python's standard lib
+import unittest
+
+# from python-ldap
+import ldap.dn
+
+
+class TestDN(unittest.TestCase):
+    """
+    test ldap.functions
+    """
+
+    def test_is_dn(self):
+        """
+        test function is_dn()
+        """
+        self.assertEquals(ldap.dn.is_dn('foobar,ou=ae-dir'), False)
+        self.assertEquals(ldap.dn.is_dn('uid=xkcd,cn=foobar,ou=ae-dir'), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
