d8ada28247b6924f8a4a961c46f59951cf937715
stroeder <stroeder>
2016-11-11 14:42:18 +0000

    added dummy method logging_file_class.flush() because LDAPObject.unbind_ext_s() invokes it

--- Lib/ldap/logger.py
+++ Lib/ldap/logger.py
@@ -13,4 +13,7 @@ class logging_file_class:
   def write(self,msg):
     logging.log(self._logging_level,msg[:-1])
 
+  def flush(self):
+    return
+
 logging_file_obj = logging_file_class(logging.DEBUG)
