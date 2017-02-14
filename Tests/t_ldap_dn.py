bec33ba407fbfd5c2d943f7abc71bc80496fe8c4
stroeder <stroeder>
2017-02-14 18:02:20 +0000

    new test-cases in t_ldap_dn.py

--- Tests/t_ldap_dn.py
+++ Tests/t_ldap_dn.py
@@ -20,8 +20,17 @@ class TestDN(unittest.TestCase):
         test function is_dn()
         """
         self.assertEquals(ldap.dn.is_dn('foobar,ou=ae-dir'), False)
+        self.assertEquals(ldap.dn.is_dn('-cn=foobar,ou=ae-dir'), False)
+        self.assertEquals(ldap.dn.is_dn(';cn=foobar,ou=ae-dir'), False)
+        self.assertEquals(ldap.dn.is_dn(',cn=foobar,ou=ae-dir'), False)
+        self.assertEquals(ldap.dn.is_dn('cn=foobar,ou=ae-dir,'), False)
         self.assertEquals(ldap.dn.is_dn('uid=xkcd,cn=foobar,ou=ae-dir'), True)
-
+        self.assertEquals(
+            ldap.dn.is_dn(
+                'cn=\xc3\xa4\xc3\xb6\xc3\xbc\xc3\x84\xc3\x96\xc3\x9c.o=\xc3\xa4\xc3\xb6\xc3\xbc\xc3\x84\xc3\x96\xc3\x9c\xc3\x9f'
+            ),
+            True
+        )
 
 if __name__ == '__main__':
     unittest.main()
