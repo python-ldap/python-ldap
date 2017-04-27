060e28adfa930dc0845d97911095217643a87fa4
stroeder <stroeder>
2017-04-27 08:41:28 +0000

    t_search was renamed to t_ldapobject

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -2,13 +2,13 @@
 python-ldap tests module package
 """
 
-import t_cext 
-import t_ldap_dn 
-import t_ldap_filter 
-import t_ldap_functions 
-import t_ldap_modlist 
-import t_ldap_schema_tokenizer 
-import t_ldapurl 
-import t_ldif 
-import t_search 
+import t_cext
+import t_ldap_dn
+import t_ldap_filter
+import t_ldap_functions
+import t_ldap_modlist
+import t_ldap_schema_tokenizer
+import t_ldapurl
+import t_ldif
+import t_ldapobject
 import t_ldap_schema_subentry
