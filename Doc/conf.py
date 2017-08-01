3e7aa46115e1dea872341144fba984bd7f5a6397
stroeder <stroeder>
2017-08-01 17:50:51 +0000

    bumped doc release to 2.4.41

--- Doc/conf.py
+++ Doc/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default value; values that are commented out
 # serve to show the default value.
 #
-# $Id: conf.py,v 1.26 2017/01/06 15:06:24 stroeder Exp $
+# $Id: conf.py,v 1.27 2017/08/01 17:50:51 stroeder Exp $
 
 import sys
 
@@ -44,7 +44,7 @@ copyright = '2008-2017, python-ldap project team'
 # The short X.Y version.
 version = '2.4'
 # The full version, including alpha/beta/rc tags.
-release = '2.4.27.0'
+release = '2.4.41.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
