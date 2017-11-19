fa1f5e4db416da7f2e2f6fc35ef71768bded0379
stroeder <stroeder>
2017-11-19 16:35:14 +0000

    a bit of PEP-8 for ldap.__init__

--- Lib/ldap/__init__.py
+++ Lib/ldap/__init__.py
@@ -6,95 +6,117 @@ See https://www.python-ldap.org/ for details.
 
 # This is also the overall release version number
 
-from pkginfo import __version__, __author__, __license__
-
 import sys
 
-if __debug__:
-  # Tracing is only supported in debugging mode
-  import traceback
-  _trace_level = 0
-  _trace_file = sys.stderr
-  _trace_stack_limit = None
+from ldap.pkginfo import __version__, __author__, __license__
 
-from ldap.pkginfo import __version__
+if __debug__:
+    # Tracing is only supported in debugging mode
+    import traceback
+    _trace_level = 0
+    _trace_file = sys.stderr
+    _trace_stack_limit = None
 
 import _ldap
-assert _ldap.__version__==__version__, \
-       ImportError('ldap %s and _ldap %s version mismatch!' % (__version__,_ldap.__version__))
+assert _ldap.__version__ == __version__, ImportError(
+    'ldap %s and _ldap %s version mismatch!' % (__version__, _ldap.__version__)
+)
 from _ldap import *
 # call into libldap to initialize it right now
+from functions import open, initialize, init, get_option, set_option
+from functions import escape_str, strf_secs, strp_secs
+from ldapobject import NO_UNIQUE_ENTRY
+from ldap.dn import explode_dn, explode_rdn
+
+
 LIBLDAP_API_INFO = _ldap.get_option(_ldap.OPT_API_INFO)
 
 OPT_NAMES_DICT = {}
-for k,v in vars(_ldap).items():
-  if k.startswith('OPT_'):
-    OPT_NAMES_DICT[v]=k
+for key, val in vars(_ldap).items():
+    if key.startswith('OPT_'):
+        OPT_NAMES_DICT[val] = key
+
 
 class DummyLock:
-  """Define dummy class with methods compatible to threading.Lock"""
-  def __init__(self):
-    pass
-  def acquire(self):
-    pass
-  def release(self):
-    pass
+    """
+    Define dummy class with methods compatible to threading.Lock
+    """
+
+    def __init__(self):
+        pass
+
+    def acquire(self):
+        """
+        dummy
+        """
+        pass
+
+    def release(self):
+        """
+        dummy
+        """
+        pass
+
 
 try:
-  # Check if Python installation was build with thread support
-  import thread
+    # Check if Python installation was build with thread support
+    import thread
 except ImportError:
-  LDAPLockBaseClass = DummyLock
+    LDAPLockBaseClass = DummyLock
 else:
-  import threading
-  LDAPLockBaseClass = threading.Lock
+    import threading
+    LDAPLockBaseClass = threading.Lock
 
 
-class LDAPLock:
-  """
-  Mainly a wrapper class to log all locking events.
-  Note that this cumbersome approach with _lock attribute was taken
-  since threading.Lock is not suitable for sub-classing.
-  """
-  _min_trace_level = 3
-
-  def __init__(self,lock_class=None,desc=''):
+class LDAPLock(object):
     """
-    lock_class
-        Class compatible to threading.Lock
-    desc
-        Description shown in debug log messages
+    Mainly a wrapper class to log all locking events.
+    Note that this cumbersome approach with _lock attribute was taken
+    since threading.Lock is not suitable for sub-classing.
     """
-    self._desc = desc
-    self._lock = (lock_class or LDAPLockBaseClass)()
+    _min_trace_level = 3
 
-  def acquire(self):
-    if __debug__:
-      global _trace_level
-      if _trace_level>=self._min_trace_level:
-        _trace_file.write('***%s.acquire() %s %s\n' % (self.__class__.__name__,repr(self),self._desc))
-    return self._lock.acquire()
+    def __init__(self, lock_class=None, desc='', trace_level=None):
+        """
+        lock_class
+            Class compatible to threading.Lock
+        desc
+            Description shown in debug log messages
+        """
+        self._desc = desc
+        self._lock = (lock_class or LDAPLockBaseClass)()
+        if trace_level is not None:
+            self._min_trace_level = trace_level
 
-  def release(self):
-    if __debug__:
-      global _trace_level
-      if _trace_level>=self._min_trace_level:
-        _trace_file.write('***%s.release() %s %s\n' % (self.__class__.__name__,repr(self),self._desc))
-    return self._lock.release()
+    def acquire(self):
+        """
+        acquire lock and log
+        """
+        if __debug__:
+            global _trace_level
+            if _trace_level >= self._min_trace_level:
+                _trace_file.write('***%s.acquire() %r %s\n' % (
+                    self.__class__.__name__, self, self._desc
+                ))
+        return self._lock.acquire()
+
+    def release(self):
+        """
+        release lock and log
+        """
+        if __debug__:
+            global _trace_level
+            if _trace_level >= self._min_trace_level:
+                _trace_file.write('***%s.release() %r %s\n' % (
+                    self.__class__.__name__, self, self._desc
+                ))
+        return self._lock.release()
 
 
 # Create module-wide lock for serializing all calls into underlying LDAP lib
 _ldap_module_lock = LDAPLock(desc='Module wide')
 
-from functions import open,initialize,init,get_option,set_option,escape_str,strf_secs,strp_secs
-
-from ldapobject import NO_UNIQUE_ENTRY
-
-from ldap.dn import explode_dn,explode_rdn,str2dn,dn2str
-del str2dn
-del dn2str
-
 # More constants
 
 # For compability of 2.3 and 2.4 OpenLDAP API
-OPT_DIAGNOSTIC_MESSAGE = OPT_ERROR_STRING
+OPT_DIAGNOSTIC_MESSAGE = _ldap.OPT_ERROR_STRING
