aedfdd1e15633eb850e7dc316afe6318161204db
Petr Viktorin <pviktori@redhat.com>
2017-11-22 13:28:47 +0100

    Add _ldap.__version__
    Cherry-picked from: b837b54 stripped trailing spaces from C source files

--- Modules/ldapmodule.c
+++ Modules/ldapmodule.c
@@ -1,13 +1,27 @@
 /* See https://www.python-ldap.org/ for details. */
 
 #include "common.h"
-#include "version.h"
 #include "constants.h"
 #include "errors.h"
 #include "functions.h"
 #include "ldapcontrol.h"
 
 #include "LDAPObject.h"
+
+#define _STR(x)        #x
+#define STR(x) _STR(x)
+
+static char version_str[] = STR(LDAPMODULE_VERSION);
+
+void
+LDAPinit_version( PyObject* d )
+{
+       PyObject *version;
+
+       version = PyString_FromString(version_str);
+       PyDict_SetItemString( d, "__version__", version );
+       Py_DECREF(version);
+}
 
 DL_EXPORT(void) init_ldap(void);
 
