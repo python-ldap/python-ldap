c3876a07f9b6f0238c945dee8911904444a18177
Christian Heimes <cheimes@redhat.com>
2017-11-23 12:23:35 +0100

    Check for LDAP schema and slapd binaries
    Integration tests depend on openldap client and server binaries. Fail
    with a human readable message in case a tool or schema is missing.
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- Tests/t_bind.py
+++ Tests/t_bind.py
@@ -13,23 +13,22 @@ import ldap, unittest
 from slapdtest import SlapdObject
 from ldap.ldapobject import LDAPObject
 
-server = None
-
-
 class TestBinds(unittest.TestCase):
 
-    def setUp(self):
-        global server
-        if server is None:
-            server = SlapdObject()
-            server.start()
+    @classmethod
+    def setUpClass(cls):
+        cls.server = SlapdObject()
+        cls.server.start()
 
-        self.server = server
-        self.unicode_val = "abc\U0001f498def"
-        self.unicode_val_bytes = self.unicode_val.encode('utf-8')
+        cls.unicode_val = "abc\U0001f498def"
+        cls.unicode_val_bytes = cls.unicode_val.encode('utf-8')
 
-        self.dn_unicode = "CN=" + self.unicode_val
-        self.dn_bytes = self.dn_unicode.encode('utf-8')
+        cls.dn_unicode = "CN=" + cls.unicode_val
+        cls.dn_bytes = cls.dn_unicode.encode('utf-8')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.stop()
 
     def _get_ldapobject(self, bytes_mode=None):
         l = LDAPObject(self.server.ldap_uri, bytes_mode=bytes_mode)
