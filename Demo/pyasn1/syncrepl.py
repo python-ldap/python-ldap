4657710b445dcc54112034b1907acea64f2e9b98
Christian Heimes <christian@python.org>
2017-12-05 13:40:48 +0100

    Use uniform shebang in all demos
    Demo scripts used different style of shebangs. The shebang
    "#!/usr/bin/env python" is preferred because it works nicely in virtual
    envs.
    
    https://github.com/python-ldap/python-ldap/pull/97
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- Demo/pyasn1/syncrepl.py
+++ Demo/pyasn1/syncrepl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 This script implements a syncrepl consumer which syncs data from an OpenLDAP
--- Demo/simplebrowse.py
+++ Demo/simplebrowse.py
@@ -1,4 +1,4 @@
-#! python
+#!/usr/bin/env python
 
 #
 # simple LDAP server browsing example
