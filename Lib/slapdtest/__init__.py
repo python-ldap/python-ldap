c09142a4afe00645f6ebde7c680d3e2d682374ca
Petr Viktorin <pviktori@redhat.com>
2017-12-11 14:33:35 +0100

    Bump version to 3.0.0b2

--- Lib/slapdtest/__init__.py
+++ Lib/slapdtest/__init__.py
@@ -5,7 +5,7 @@ slapdtest - module for spawning test instances of OpenLDAP's slapd server
 See https://www.python-ldap.org/ for details.
 """
 
-__version__ = '3.0.0b1'
+__version__ = '3.0.0b2'
 
 from slapdtest._slapdtest import SlapdObject, SlapdTestCase, SysLogHandler
 from slapdtest._slapdtest import skip_unless_ci, requires_sasl, requires_tls
