adf47613f2df5a9b080d00f9b2632da556a61ea8
Petr Viktorin <pviktori@redhat.com>
2019-03-12 16:01:16 +0100

    Bump version to 3.2.0 and update the changelog

--- Lib/ldapurl.py
+++ Lib/ldapurl.py
@@ -4,7 +4,7 @@ ldapurl - handling of LDAP URLs as described in RFC 4516
 See https://www.python-ldap.org/ for details.
 """
 
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 
 __all__ = [
   # constants
