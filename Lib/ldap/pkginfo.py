dae0c5193d02709df6f6918d0e495180a1bff0c8
Petr Viktorin <pviktori@redhat.com>
2020-06-29 16:43:42 +0200

    Prepare release 3.3.1 (#365)

--- Lib/ldap/pkginfo.py
+++ Lib/ldap/pkginfo.py
@@ -2,6 +2,6 @@
 """
 meta attributes for packaging which does not import any dependencies
 """
-__version__ = '3.3.0'
+__version__ = '3.3.1'
 __author__ = u'python-ldap project'
 __license__ = 'Python style'
