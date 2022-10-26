c0a33764ea6efb408af6c37844ec3e5e17eec603
Josh <josh@joshthomas.dev>
2022-10-26 16:00:00 -0500

    Add 3.10 and 3.11 to ci and tox

--- setup.py
+++ setup.py
@@ -90,6 +90,8 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
     # Note: when updating Python versions, also change tox.ini and .github/workflows/*
 
     'Topic :: Database',
