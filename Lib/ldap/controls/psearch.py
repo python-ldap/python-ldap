d1d99e53a529c8544b367528754f6e15a8061887
David Härdeman <david@hardeman.nu>
2024-01-26 12:30:25 +0100

    [python-ldap] Type fixes for Lib/ldap/controls/psearch.py
    First, change class PersistentSearchControl(RequestControl) so that
    self.changeTypes is always a List[str] instead of List[str|int].
    
    Second, in class EntryChangeNotificationControl, change decodeControlValue()
    to not return a value (see the ResponseControl parent class).

--- Lib/ldap/controls/psearch.py
+++ Lib/ldap/controls/psearch.py
@@ -60,17 +60,20 @@ class PersistentSearchControl(RequestControl):
   def __init__(self,criticality=True,changeTypes=None,changesOnly=False,returnECs=True):
     self.criticality,self.changesOnly,self.returnECs = \
       criticality,changesOnly,returnECs
-    self.changeTypes = changeTypes or CHANGE_TYPES_INT.values()
+    if isinstance(changeTypes, int):
+        changeTypes = [changeTypes]
+    self.changeTypes = changeTypes or CHANGE_TYPES_INT.keys()
 
-  def encodeControlValue(self):
-    if not type(self.changeTypes)==type(0):
-      # Assume a sequence type of integers to be OR-ed
-      changeTypes_int = 0
-      for ct in self.changeTypes:
-        changeTypes_int |= CHANGE_TYPES_INT.get(ct, ct)
-      self.changeTypes = changeTypes_int
+  def encodeControlValue(self) -> bytes:
+    # Assume a sequence type of names of integers to be OR-ed
+    changeTypes_int = 0
+    for ct in self.changeTypes:
+      if isinstance(ct, str):
+          ct = CHANGE_TYPES_INT[ct]
+      changeTypes_int |= ct
+
     p = self.PersistentSearchControlValue()
-    p.setComponentByName('changeTypes',univ.Integer(self.changeTypes))
+    p.setComponentByName('changeTypes',univ.Integer(changeTypes_int))
     p.setComponentByName('changesOnly',univ.Boolean(self.changesOnly))
     p.setComponentByName('returnECs',univ.Boolean(self.returnECs))
     return encoder.encode(p)
@@ -124,6 +127,5 @@ class EntryChangeNotificationControl(ResponseControl):
       self.changeNumber = int(changeNumber)
     else:
       self.changeNumber = None
-    return (self.changeType,self.previousDN,self.changeNumber)
 
 KNOWN_RESPONSE_CONTROLS[EntryChangeNotificationControl.controlType] = EntryChangeNotificationControl
