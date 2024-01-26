8cffd595fe5f10e640fe912fb3b0c74884c9a20f
David Härdeman <david@hardeman.nu>
2024-01-26 11:40:15 +0100

    [python-ldap] Make __version__ imports more consistent
    This is also in preparation for the typing patches as it helps to avoid
    import loops.

--- Lib/ldap/schema/__init__.py
+++ Lib/ldap/schema/__init__.py
@@ -4,7 +4,7 @@ ldap.schema -  LDAPv3 schema handling
 See https://www.python-ldap.org/ for details.
 """
 
-from ldap import __version__
+from ldap.pkginfo import __version__
 
 from ldap.schema.subentry import SubSchema,SCHEMA_ATTRS,SCHEMA_CLASS_MAPPING,SCHEMA_ATTR_MAPPING,urlfetch
 from ldap.schema.models import *
