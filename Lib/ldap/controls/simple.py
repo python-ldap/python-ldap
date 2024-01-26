d19ee4fc8a5d8d73907afda60e3661bcea416e4e
David Härdeman <david@hardeman.nu>
2024-01-26 13:17:24 +0100

    [python-ldap] Type fixes for Lib/ldap/controls/simple.py
    The comments for the ProxyAuthzControl and AuthorizationIdentityResponseControl
    classes indicate that they expect/return a str (which makes sense), but then
    fail to actually deal with the str properly.
    
    The GetEffectiveRightsControl class has a similar issue (but seems to be a stub
    at the moment, the "authzId" parameter should probably be renamed "gerSubject"
    and can take much more than just a DN).

--- Lib/ldap/controls/simple.py
+++ Lib/ldap/controls/simple.py
@@ -106,7 +106,11 @@ class ProxyAuthzControl(RequestControl):
   """
 
   def __init__(self,criticality,authzId):
-    RequestControl.__init__(self,ldap.CONTROL_PROXY_AUTHZ,criticality,authzId)
+    if isinstance(authzId, str):
+        encoded = authzId.encode('utf-8')
+    else:
+        encoded = authzId
+    RequestControl.__init__(self,ldap.CONTROL_PROXY_AUTHZ,criticality,encoded)
 
 
 class AuthorizationIdentityRequestControl(ValueLessRequestControl):
@@ -131,7 +135,7 @@ class AuthorizationIdentityResponseControl(ResponseControl):
   controlType = '2.16.840.1.113730.3.4.15'
 
   def decodeControlValue(self,encodedControlValue):
-    self.authzId = encodedControlValue
+    self.authzId = encodedControlValue.decode('utf-8')
 
 
 KNOWN_RESPONSE_CONTROLS[AuthorizationIdentityResponseControl.controlType] = AuthorizationIdentityResponseControl
@@ -141,6 +145,11 @@ class GetEffectiveRightsControl(RequestControl):
   """
   Get Effective Rights Control
   """
+  controlType = '1.3.6.1.4.1.42.2.27.9.5.2'
 
   def __init__(self,criticality,authzId=None):
-    RequestControl.__init__(self,'1.3.6.1.4.1.42.2.27.9.5.2',criticality,authzId)
+    if isinstance(authzId, str):
+        encoded = authzId.encode('utf-8')
+    else:
+        encoded = authzId
+    RequestControl.__init__(self,self.controlType,criticality,encoded)
