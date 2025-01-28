e628f1582b46269ba6c31e1e3ee8a952cb32bb7d
Simon Pichugin <simon.pichugin@gmail.com>
2025-01-28 14:26:58 -0800

    Deprecate EOL Python 3.6, 3.7, 3.8 versions

--- setup.py
+++ setup.py
@@ -86,9 +86,6 @@ setup(
 
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
@@ -160,6 +157,6 @@ setup(
     'pyasn1_modules >= 0.1.5',
   ],
   zip_safe=False,
-  python_requires='>=3.6',
+  python_requires='>=3.9',
   test_suite = 'Tests',
 )
