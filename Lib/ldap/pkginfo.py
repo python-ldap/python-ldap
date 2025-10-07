0c50a6f2f9add47d814db63364b74a09218e3f59
Simon Pichugin <simon.pichugin@gmail.com>
2025-10-06 19:39:49 -0700

    Prepare a new release
    Disable Python 3.6, 3.7 CI workflow
    as it's supported on Ubuntu 22.04
    Update GH Workflows.

--- Lib/ldap/pkginfo.py
+++ Lib/ldap/pkginfo.py
@@ -1,6 +1,6 @@
 """
 meta attributes for packaging which does not import any dependencies
 """
-__version__ = '3.4.4'
+__version__ = '3.4.5'
 __author__ = 'python-ldap project'
 __license__ = 'Python style'
--- Lib/slapdtest/__init__.py
+++ Lib/slapdtest/__init__.py
@@ -4,7 +4,7 @@ slapdtest - module for spawning test instances of OpenLDAP's slapd server
 See https://www.python-ldap.org/ for details.
 """
 
-__version__ = '3.4.4'
+__version__ = '3.4.5'
 
 from slapdtest._slapdtest import SlapdObject, SlapdTestCase, SysLogHandler
 from slapdtest._slapdtest import requires_ldapi, requires_sasl, requires_tls
