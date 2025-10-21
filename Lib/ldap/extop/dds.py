python-ldap/python-ldap

commit 414ae1de91543a1c0fee0738f97fe1a33d0fe666

fix(extop.dds): fix unset RefreshRequest.requestValue
>>> from ldap.extop.dds import RefreshRequest
>>> req = RefreshRequest(RefreshRequest.requestName, 'uid=temp,dc=freeiam,dc=org', 86400)
>>> repr(req)
Traceback (most recent call last):
  File "<stdin>", line 1, in <module>
  File "/usr/lib/python3/dist-packages/ldap/extop/__init__.py", line 31, in __repr__
    return f'{self.__class__.__name__}({self.requestName},{self.requestValue})'
                                                           ^^^^^^^^^^^^^^^^^
AttributeError: 'RefreshRequest' object has no attribute 'requestValue'. Did you mean: 'requestName'?

Lib/ldap/extop/dds.py
@@ -35,6 +35,7 @@ class RefreshRequest(ExtendedRequest):
     )
 
   def __init__(self,requestName=None,entryName=None,requestTtl=None):
+    super().__init__(requestName, b'')
     self.entryName = entryName
     self.requestTtl = requestTtl or self.defaultRequestTtl
 

commit 51ac5a8d037b7e23c4a731c796a425f2accd72d6

fix(extop.dds): make passing of requestName optional again
requestName is already set at class member. It seems there is code out
there which is not giving it as value but passing None.
in that case, fallback to the class member.

Fixes: 414ae1de91543a1c0fee0738f97fe1a33d0fe666

Lib/ldap/extop/dds.py
@@ -35,7 +35,7 @@ class RefreshRequest(ExtendedRequest):
     )
 
   def __init__(self,requestName=None,entryName=None,requestTtl=None):
-    super().__init__(requestName, b'')
+    super().__init__(requestName or self.requestName, b'')
     self.entryName = entryName
     self.requestTtl = requestTtl or self.defaultRequestTtl
 