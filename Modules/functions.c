ca8cd1c9766bb5bc6c43a809b0df95768e831fd9
Ondřej Kuzník <ondra@mistotebe.net>
2026-05-13 13:58:02 +0100

    fix(_ldap): correct handling of empty string in _ldap.str2dn
    Fixes: https://github.com/python-ldap/python-ldap/issues/549

--- Modules/functions.c
+++ Modules/functions.c
@@ -103,7 +103,7 @@ l_ldap_str2dn(PyObject *unused, PyObject *args)
      * ((('a','b',1),('c','d',1)),(('e','f',1),))
      * The integers are a bit combination of the AVA_* flags
      */
-    if (!PyArg_ParseTuple(args, "z#|i:str2dn", &str.bv_val, &str_len, &flags))
+    if (!PyArg_ParseTuple(args, "s#|i:str2dn", &str.bv_val, &str_len, &flags))
         return NULL;
     str.bv_len = (ber_len_t) str_len;
 
@@ -115,7 +115,7 @@ l_ldap_str2dn(PyObject *unused, PyObject *args)
     if (!tmp)
         goto failed;
 
-    for (i = 0; dn[i]; i++) {
+    for (i = 0; dn && dn[i]; i++) {
         LDAPRDN rdn;
         PyObject *rdnlist;
 
