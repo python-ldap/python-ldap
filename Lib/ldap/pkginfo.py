967cb64c92b5d41e9a0e8632d1f22c8f78fee7e7
Ondřej Kuzník <ondra@mistotebe.net>
2026-05-19 14:59:25 +0100

    chore: Prepare a new release 3.4.7

--- Lib/ldap/pkginfo.py
+++ Lib/ldap/pkginfo.py
@@ -1,6 +1,6 @@
 """
 meta attributes for packaging which does not import any dependencies
 """
-__version__ = '3.4.6'
+__version__ = '3.4.7'
 __author__ = 'python-ldap project'
 __license__ = 'Python style'
