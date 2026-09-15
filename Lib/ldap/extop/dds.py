python-ldap/python-ldap

commit a4110a35af181650bea81d2bcb5ecedd6083adec

[python-ldap] Add type annotations
Phew, after all the prep work, the actual type annotations can finally be added.

Lib/ldap/extop/dds.py
@@ -5,6 +5,7 @@ ldap.extop.dds - Classes for Dynamic Entries extended operations
 See https://www.python-ldap.org/ for details.
 """
 
+from __future__ import annotations
 from ldap.extop import ExtendedRequest,ExtendedResponse
 
 # Imports from pyasn1
@@ -13,12 +14,13 @@ from pyasn1.codec.der import encoder,decoder
 from pyasn1_modules.rfc2251 import LDAPDN
 
 
+
 class RefreshRequest(ExtendedRequest):
 
   requestName = '1.3.6.1.4.1.1466.101.119.1'
   defaultRequestTtl = 86400
 
-  class RefreshRequestValue(univ.Sequence):
+  class RefreshRequestValue(univ.Sequence):  # type: ignore
     componentType = namedtype.NamedTypes(
       namedtype.NamedType(
         'entryName',
@@ -34,12 +36,17 @@ class RefreshRequest(ExtendedRequest):
       ),
     )
 
-  def __init__(self,requestName=None,entryName=None,requestTtl=None):
+  def __init__(
+    self,
+    requestName: str | None = None,
+    entryName: str | None = None,
+    requestTtl: int | None = None
+  ) -> None:
     super().__init__(requestName or self.requestName, b'')
     self.entryName = entryName
     self.requestTtl = requestTtl or self.defaultRequestTtl
 
-  def encodedRequestValue(self):
+  def encodedRequestValue(self) -> bytes:
     p = self.RefreshRequestValue()
     p.setComponentByName(
       'entryName',
@@ -53,13 +60,13 @@ class RefreshRequest(ExtendedRequest):
         implicitTag=tag.Tag(tag.tagClassContext,tag.tagFormatSimple,1)
       )
     )
-    return encoder.encode(p)
+    return encoder.encode(p)  # type: ignore[no-any-return]
 
 
 class RefreshResponse(ExtendedResponse):
   responseName = '1.3.6.1.4.1.1466.101.119.1'
 
-  class RefreshResponseValue(univ.Sequence):
+  class RefreshResponseValue(univ.Sequence):  # type: ignore
     componentType = namedtype.NamedTypes(
       namedtype.NamedType(
         'responseTtl',
@@ -69,7 +76,7 @@ class RefreshResponse(ExtendedResponse):
       )
     )
 
-  def decodeResponseValue(self,value):
+  def decodeResponseValue(self, value: bytes | None) -> int:
     respValue,_ = decoder.decode(value,asn1Spec=self.RefreshResponseValue())
     self.responseTtl = int(respValue.getComponentByName('responseTtl'))
     return self.responseTtl

Lib/_ldap.pyi
@@ -1,0 +1,406 @@
+from typing import Any, ClassVar
+
+__version__: str
+__license__: str
+__author__: str
+API_VERSION: int
+AUTH_NONE: int
+AUTH_SIMPLE: int
+AVA_BINARY: int
+AVA_NONPRINTABLE: int
+AVA_NULL: int
+AVA_STRING: int
+CONTROL_ASSERT: str
+CONTROL_MANAGEDSAIT: str
+CONTROL_PAGEDRESULTS: str
+CONTROL_PASSWORDPOLICYREQUEST: str
+CONTROL_PASSWORDPOLICYRESPONSE: str
+CONTROL_POST_READ: str
+CONTROL_PRE_READ: str
+CONTROL_PROXY_AUTHZ: str
+CONTROL_RELAX: str
+CONTROL_SORTREQUEST: str
+CONTROL_SORTRESPONSE: str
+CONTROL_SUBENTRIES: str
+CONTROL_SYNC: str
+CONTROL_SYNC_DONE: str
+CONTROL_SYNC_STATE: str
+CONTROL_VALUESRETURNFILTER: str
+DEREF_ALWAYS: int
+DEREF_FINDING: int
+DEREF_NEVER: int
+DEREF_SEARCHING: int
+DN_FORMAT_AD_CANONICAL: int
+DN_FORMAT_DCE: int
+DN_FORMAT_LDAP: int
+DN_FORMAT_LDAPV2: int
+DN_FORMAT_LDAPV3: int
+DN_FORMAT_MASK: int
+DN_FORMAT_UFN: int
+DN_PEDANTIC: int
+DN_PRETTY: int
+DN_P_NOLEADTRAILSPACES: int
+DN_P_NOSPACEAFTERRDN: int
+DN_SKIP: int
+INIT_FD_AVAIL: int
+LIBLDAP_R: int
+MOD_ADD: int
+MOD_BVALUES: int
+MOD_DELETE: int
+MOD_INCREMENT: int
+MOD_REPLACE: int
+MSG_ALL: int
+MSG_ONE: int
+MSG_RECEIVED: int
+NO_LIMIT: int
+OPT_API_FEATURE_INFO: int
+OPT_API_INFO: int
+OPT_CLIENT_CONTROLS: int
+OPT_CONNECT_ASYNC: int
+OPT_DEBUG_LEVEL: int
+OPT_DEFBASE: int
+OPT_DEREF: int
+OPT_DESC: int
+OPT_DIAGNOSTIC_MESSAGE: int
+OPT_ERROR_NUMBER: int
+OPT_ERROR_STRING: int
+OPT_HOST_NAME: int
+OPT_MATCHED_DN: int
+OPT_NETWORK_TIMEOUT: int
+OPT_OFF: int
+OPT_ON: int
+OPT_PROTOCOL_VERSION: int
+OPT_REFERRALS: int
+OPT_REFHOPLIMIT: int
+OPT_RESTART: int
+OPT_RESULT_CODE: int
+OPT_SERVER_CONTROLS: int
+OPT_SIZELIMIT: int
+OPT_SUCCESS: int
+OPT_TCP_USER_TIMEOUT: int
+OPT_TIMELIMIT: int
+OPT_TIMEOUT: int
+OPT_URI: int
+OPT_X_KEEPALIVE_IDLE: int
+OPT_X_KEEPALIVE_INTERVAL: int
+OPT_X_KEEPALIVE_PROBES: int
+OPT_X_SASL_AUTHCID: int
+OPT_X_SASL_AUTHZID: int
+OPT_X_SASL_MECH: int
+OPT_X_SASL_NOCANON: int
+OPT_X_SASL_REALM: int
+OPT_X_SASL_SECPROPS: int
+OPT_X_SASL_SSF: int
+OPT_X_SASL_SSF_EXTERNAL: int
+OPT_X_SASL_SSF_MAX: int
+OPT_X_SASL_SSF_MIN: int
+OPT_X_SASL_USERNAME: int
+OPT_X_TLS: int
+OPT_X_TLS_ALLOW: int
+OPT_X_TLS_CACERTDIR: int
+OPT_X_TLS_CACERTFILE: int
+OPT_X_TLS_CERTFILE: int
+OPT_X_TLS_CIPHER: int
+OPT_X_TLS_CIPHER_SUITE: int
+OPT_X_TLS_CRLCHECK: int
+OPT_X_TLS_CRLFILE: int
+OPT_X_TLS_CRL_ALL: int
+OPT_X_TLS_CRL_NONE: int
+OPT_X_TLS_CRL_PEER: int
+OPT_X_TLS_CTX: int
+OPT_X_TLS_DEMAND: int
+OPT_X_TLS_DHFILE: int
+OPT_X_TLS_ECNAME: int
+OPT_X_TLS_HARD: int
+OPT_X_TLS_KEYFILE: int
+OPT_X_TLS_NEVER: int
+OPT_X_TLS_NEWCTX: int
+OPT_X_TLS_PACKAGE: int
+OPT_X_TLS_PEERCERT: int
+OPT_X_TLS_PROTOCOL_MAX: int
+OPT_X_TLS_PROTOCOL_MIN: int
+OPT_X_TLS_PROTOCOL_SSL3: int
+OPT_X_TLS_PROTOCOL_TLS1_0: int
+OPT_X_TLS_PROTOCOL_TLS1_1: int
+OPT_X_TLS_PROTOCOL_TLS1_2: int
+OPT_X_TLS_PROTOCOL_TLS1_3: int
+OPT_X_TLS_RANDOM_FILE: int
+OPT_X_TLS_REQUIRE_CERT: int
+OPT_X_TLS_REQUIRE_SAN: int
+OPT_X_TLS_TRY: int
+OPT_X_TLS_VERSION: int
+PORT: int
+REQ_ABANDON: int
+REQ_ADD: int
+REQ_BIND: int
+REQ_COMPARE: int
+REQ_DELETE: int
+REQ_EXTENDED: int
+REQ_MODIFY: int
+REQ_MODRDN: int
+REQ_SEARCH: int
+REQ_UNBIND: int
+RES_ADD: int
+RES_ANY: int
+RES_BIND: int
+RES_COMPARE: int
+RES_DELETE: int
+RES_EXTENDED: int
+RES_INTERMEDIATE: int
+RES_MODIFY: int
+RES_MODRDN: int
+RES_SEARCH_ENTRY: int
+RES_SEARCH_REFERENCE: int
+RES_SEARCH_RESULT: int
+RES_UNSOLICITED: int
+SASL_AUTOMATIC: int
+SASL_AVAIL: int
+SASL_INTERACTIVE: int
+SASL_QUIET: int
+SCOPE_BASE: int
+SCOPE_ONELEVEL: int
+SCOPE_SUBORDINATE: int
+SCOPE_SUBTREE: int
+SYNC_INFO: str
+TAG_CONTROLS: int
+TAG_EXOP_REQ_OID: int
+TAG_EXOP_REQ_VALUE: int
+TAG_EXOP_RES_OID: int
+TAG_EXOP_RES_VALUE: int
+TAG_LDAPCRED: int
+TAG_LDAPDN: int
+TAG_MESSAGE: int
+TAG_MSGID: int
+TAG_NEWSUPERIOR: int
+TAG_REFERRAL: int
+TAG_SASL_RES_CREDS: int
+TLS_AVAIL: int
+URL_ERR_BADSCOPE: int
+URL_ERR_MEM: int
+VENDOR_VERSION: int
+VERSION: int
+VERSION1: int
+VERSION2: int
+VERSION3: int
+VERSION_MAX: int
+VERSION_MIN: int
+
+class LDAPError(Exception): ...
+
+class ADMINLIMIT_EXCEEDED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class AFFECTS_MULTIPLE_DSAS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class ALIAS_DEREF_PROBLEM(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class ALIAS_PROBLEM(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class ALREADY_EXISTS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class ASSERTION_FAILED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class AUTH_METHOD_NOT_SUPPORTED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class AUTH_UNKNOWN(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class BUSY(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class CANCELLED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class CANNOT_CANCEL(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class CLIENT_LOOP(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class COMPARE_FALSE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class COMPARE_TRUE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class CONFIDENTIALITY_REQUIRED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class CONNECT_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class CONSTRAINT_VIOLATION(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class CONTROL_NOT_FOUND(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class DECODING_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class ENCODING_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class FILTER_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class INAPPROPRIATE_AUTH(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class INAPPROPRIATE_MATCHING(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class INSUFFICIENT_ACCESS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class INVALID_CREDENTIALS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class INVALID_DN_SYNTAX(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class INVALID_SYNTAX(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class IS_LEAF(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class LOCAL_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class LOOP_DETECT(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class MORE_RESULTS_TO_RETURN(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NAMING_VIOLATION(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NOT_ALLOWED_ON_NONLEAF(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NOT_ALLOWED_ON_RDN(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NOT_SUPPORTED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NO_MEMORY(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NO_OBJECT_CLASS_MODS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NO_RESULTS_RETURNED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NO_SUCH_ATTRIBUTE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NO_SUCH_OBJECT(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class NO_SUCH_OPERATION(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class OBJECT_CLASS_VIOLATION(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class OPERATIONS_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class OTHER(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class PARAM_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class PARTIAL_RESULTS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class PROTOCOL_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class PROXIED_AUTHORIZATION_DENIED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class REFERRAL(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class REFERRAL_LIMIT_EXCEEDED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class RESULTS_TOO_LARGE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class SASL_BIND_IN_PROGRESS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class SERVER_DOWN(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class SIZELIMIT_EXCEEDED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class STRONG_AUTH_NOT_SUPPORTED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class STRONG_AUTH_REQUIRED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class SUCCESS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class TIMELIMIT_EXCEEDED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class TIMEOUT(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class TOO_LATE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class TYPE_OR_VALUE_EXISTS(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class UNAVAILABLE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class UNAVAILABLE_CRITICAL_EXTENSION(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class UNDEFINED_TYPE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class UNWILLING_TO_PERFORM(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class USER_CANCELLED(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class VLV_ERROR(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class X_PROXY_AUTHZ_FAILURE(LDAPError):
+    errnum: ClassVar[int] = ...
+
+class error(Exception): ...
+
+def decode_page_control(*args: Any, **kwargs: Any) -> Any: ...
+def encode_assertion_control(*args: Any, **kwargs: Any) -> Any: ...
+def encode_page_control(*args: Any, **kwargs: Any) -> Any: ...
+def encode_valuesreturnfilter_control(*args: Any, **kwargs: Any) -> Any: ...
+def get_option(*args: Any, **kwargs: Any) -> Any: ...
+def initialize(*args: Any, **kwargs: Any) -> Any: ...
+def initialize_fd(*args: Any, **kwargs: Any) -> Any: ...
+def set_option(*args: Any, **kwargs: Any) -> Any: ...
+def str2dn(*args: Any, **kwargs: Any) -> Any: ...
+def dn2str(*args: Any, **kwargs: Any) -> Any: ...
+def is_filter(filterstr: str) -> bool: ...

Lib/ldap/__init__.py
@@ -6,10 +6,15 @@ See https://www.python-ldap.org/ for details.
 
 # This is also the overall release version number
 
+from __future__ import annotations
 from ldap.pkginfo import __version__, __author__, __license__
 
 import os
 import sys
+
+from typing import Any, Type, Union
+from ldap._types import *
+
 
 if __debug__:
   # Tracing is only supported in debugging mode
@@ -45,18 +50,21 @@ for k,v in vars(_ldap).items():
 
 class DummyLock:
   """Define dummy class with methods compatible to threading.Lock"""
-  def __init__(self):
+  def __init__(self) -> None:
     pass
-  def acquire(self):
-    pass
-  def release(self):
+
+  def acquire(self) -> bool:
+    return True
+
+  def release(self) -> None:
     pass
 
 try:
   # Check if Python installation was build with thread support
+  # FIXME: This can be simplified, from Python 3.7 this module is mandatory
   import threading
 except ImportError:
-  LDAPLockBaseClass = DummyLock
+  LDAPLockBaseClass: Union[Type[DummyLock], Type[threading.Lock]] = DummyLock
 else:
   LDAPLockBaseClass = threading.Lock
 
@@ -69,7 +77,11 @@ class LDAPLock:
   """
   _min_trace_level = 3
 
-  def __init__(self,lock_class=None,desc=''):
+  def __init__(
+    self,
+    lock_class: Type[Any] | None = None,
+    desc: str = ''
+  ) -> None:
     """
     lock_class
         Class compatible to threading.Lock
@@ -79,19 +91,19 @@ class LDAPLock:
     self._desc = desc
     self._lock = (lock_class or LDAPLockBaseClass)()
 
-  def acquire(self):
+  def acquire(self) -> bool:
     if __debug__:
       global _trace_level
       if _trace_level>=self._min_trace_level:
         _trace_file.write('***{}.acquire() {} {}\n'.format(self.__class__.__name__,repr(self),self._desc))
     return self._lock.acquire()
 
-  def release(self):
+  def release(self) -> None:
     if __debug__:
       global _trace_level
       if _trace_level>=self._min_trace_level:
         _trace_file.write('***{}.release() {} {}\n'.format(self.__class__.__name__,repr(self),self._desc))
-    return self._lock.release()
+    self._lock.release()
 
 
 # Create module-wide lock for serializing all calls into underlying LDAP lib

Lib/ldap/_types.py
@@ -1,0 +1,56 @@
+"""
+types - type annotations which are shared across modules
+
+See https://www.python-ldap.org/ for details.
+"""
+from __future__ import annotations
+from ldap.pkginfo import __version__
+
+from typing import MutableMapping, Optional, Sequence, Union
+
+__all__ = [
+    'LDAPModListAddEntry',
+    'LDAPModListModifyEntry',
+    'LDAPModListEntry',
+    'LDAPAddModList',
+    'LDAPModifyModList',
+    'LDAPModList',
+    'LDAPEntryDict',
+    'LDAPControlTuple',
+    'LDAPControlTuples',
+    'LDAPSearchResult',
+]
+
+LDAPModListAddEntry = tuple[str, list[bytes]]
+"""The type of an addition entry in a modlist."""
+
+LDAPModListModifyEntry = tuple[
+    int, str, Optional[Union[bytes, list[bytes]]]
+]
+"""The type of a modification entry in a modlist."""
+
+LDAPModListEntry = Union[LDAPModListAddEntry, LDAPModListModifyEntry]
+"""The type of any kind of entry in a modlist."""
+
+LDAPAddModList = Sequence[LDAPModListAddEntry]
+"""The type of an add modlist."""
+
+LDAPModifyModList = Sequence[LDAPModListModifyEntry]
+"""The type of a modify modlist."""
+
+LDAPModList = Sequence[LDAPModListEntry]
+"""The type of a mixed modlist."""
+
+LDAPEntryDict = MutableMapping[str, list[bytes]]
+"""The type used to store attribute-value mappings for a given LDAP entry
+(attribute name, list of binary values)."""
+
+LDAPControlTuple = tuple[str, str, Optional[str]]
+"""The type used to store controls (type, criticality, value)."""
+
+LDAPControlTuples = list[LDAPControlTuple]
+"""The type used to store control lists."""
+
+LDAPSearchResult = tuple[str, LDAPEntryDict]
+"""The type of a search result, a tuple with a DN string and a dict of
+attributes."""

Lib/ldap/asyncsearch.py
@@ -3,12 +3,20 @@ ldap.asyncsearch - handle async LDAP search operations
 
 See https://www.python-ldap.org/ for details.
 """
+from __future__ import annotations
 
 import ldap
 
 from ldap.pkginfo import __version__
+from ldap.controls import RequestControl
+from typing import Any, TextIO, TYPE_CHECKING
+from collections.abc import Iterable, Sequence
+from ldap._types import LDAPSearchResult, LDAPEntryDict
 
 import ldif
+
+if TYPE_CHECKING:
+    import ldap.ldapobject
 
 SEARCH_RESULT_TYPES = {
   ldap.RES_SEARCH_ENTRY,
@@ -24,12 +32,16 @@ ENTRY_RESULT_TYPES = {
 
 class WrongResultType(Exception):
 
-  def __init__(self,receivedResultType,expectedResultTypes):
+  def __init__(
+    self,
+    receivedResultType: int | None,
+    expectedResultTypes: Iterable[int],
+  ) -> None:
     self.receivedResultType = receivedResultType
     self.expectedResultTypes = expectedResultTypes
     Exception.__init__(self)
 
-  def __str__(self):
+  def __str__(self) -> str:
     return 'Received wrong result type {} (expected one of {}).'.format(
       self.receivedResultType,
       ', '.join([str(x) for x in self.expectedResultTypes]),
@@ -46,23 +58,23 @@ class AsyncSearchHandler:
     LDAPObject instance
   """
 
-  def __init__(self,l):
+  def __init__(self, l: ldap.ldapobject.LDAPObject) -> None:
     self._l = l
-    self._msgId = None
+    self._msgId: int | None = None
     self._afterFirstResult = 1
 
   def startSearch(
     self,
-    searchRoot,
-    searchScope,
-    filterStr,
-    attrList=None,
-    attrsOnly=0,
-    timeout=-1,
-    sizelimit=0,
-    serverctrls=None,
-    clientctrls=None
-  ):
+    searchRoot: str,
+    searchScope: int,
+    filterStr: str,
+    attrList: list[str] | None = None,
+    attrsOnly: int = 0,
+    timeout: int = -1,
+    sizelimit: int = 0,
+    serverctrls: list[RequestControl] | None = None,
+    clientctrls: list[RequestControl] | None = None,
+  ) -> None:
     """
     searchRoot
         See parameter base of method LDAPObject.search()
@@ -89,26 +101,30 @@ class AsyncSearchHandler:
       attrList,attrsOnly,serverctrls,clientctrls,timeout,sizelimit
     )
     self._afterFirstResult = 1
-    return # startSearch()
 
-  def preProcessing(self):
+  def preProcessing(self) -> Any:
     """
     Do anything you want after starting search but
     before receiving and processing results
     """
 
-  def afterFirstResult(self):
+  def afterFirstResult(self) -> Any:
     """
     Do anything you want right after successfully receiving but before
     processing first result
     """
 
-  def postProcessing(self):
+  def postProcessing(self) -> Any:
     """
     Do anything you want after receiving and processing all results
     """
 
-  def processResults(self,ignoreResultsNumber=0,processResultsCount=0,timeout=-1):
+  def processResults(
+    self,
+    ignoreResultsNumber: int = 0,
+    processResultsCount: int = 0,
+    timeout: int = -1,
+  ) -> int:
     """
     ignoreResultsNumber
         Don't process the first ignoreResultsNumber results.
@@ -159,7 +175,11 @@ class AsyncSearchHandler:
     self.postProcessing()
     return partial # processResults()
 
-  def _processSingleResult(self,resultType,resultItem):
+  def _processSingleResult(
+    self,
+    resultType: int,
+    resultItem: LDAPSearchResult,
+  ) -> Any:
     """
     Process single entry
 
@@ -180,11 +200,15 @@ class List(AsyncSearchHandler):
   results.
   """
 
-  def __init__(self,l):
+  def __init__(self, l: ldap.ldapobject.LDAPObject) -> None:
     AsyncSearchHandler.__init__(self,l)
-    self.allResults = []
+    self.allResults: list[tuple[int, LDAPSearchResult]] = []
 
-  def _processSingleResult(self,resultType,resultItem):
+  def _processSingleResult(
+    self,
+    resultType: int,
+    resultItem: LDAPSearchResult,
+  ) -> None:
     self.allResults.append((resultType,resultItem))
 
 
@@ -193,11 +217,15 @@ class Dict(AsyncSearchHandler):
   Class for collecting all search results into a dictionary {dn:entry}
   """
 
-  def __init__(self,l):
+  def __init__(self, l: ldap.ldapobject.LDAPObject) -> None:
     AsyncSearchHandler.__init__(self,l)
-    self.allEntries = {}
+    self.allEntries: dict[str, LDAPEntryDict] = {}
 
-  def _processSingleResult(self,resultType,resultItem):
+  def _processSingleResult(
+    self,
+    resultType: int,
+    resultItem: LDAPSearchResult,
+  ) -> None:
     if resultType in ENTRY_RESULT_TYPES:
       # Search continuations are ignored
       dn,entry = resultItem
@@ -210,12 +238,20 @@ class IndexedDict(Dict):
   and maintain case-sensitive equality indexes to entries
   """
 
-  def __init__(self,l,indexed_attrs=None):
+  def __init__(
+    self,
+    l: ldap.ldapobject.LDAPObject,
+    indexed_attrs: Sequence[str] | None = None,
+  ) -> None:
     Dict.__init__(self,l)
     self.indexed_attrs = indexed_attrs or ()
-    self.index = {}.fromkeys(self.indexed_attrs,{})
+    self.index: dict[str, dict[bytes, list[str]]] = {}.fromkeys(self.indexed_attrs,{})
 
-  def _processSingleResult(self,resultType,resultItem):
+  def _processSingleResult(
+    self,
+    resultType: int,
+    resultItem: LDAPSearchResult,
+  ) -> None:
     if resultType in ENTRY_RESULT_TYPES:
       # Search continuations are ignored
       dn,entry = resultItem
@@ -240,20 +276,26 @@ class FileWriter(AsyncSearchHandler):
     File object instance where the LDIF data is written to
   """
 
-  def __init__(self,l,f,headerStr='',footerStr=''):
+  def __init__(
+    self,
+    l: ldap.ldapobject.LDAPObject,
+    f: TextIO,
+    headerStr: str = '',
+    footerStr: str = '',
+  ) -> None:
     AsyncSearchHandler.__init__(self,l)
     self._f = f
     self.headerStr = headerStr
     self.footerStr = footerStr
 
-  def preProcessing(self):
+  def preProcessing(self) -> None:
     """
     The headerStr is written to output after starting search but
     before receiving and processing results.
     """
     self._f.write(self.headerStr)
 
-  def postProcessing(self):
+  def postProcessing(self) -> None:
     """
     The footerStr is written to output after receiving and
     processing results.
@@ -273,14 +315,24 @@ class LDIFWriter(FileWriter):
     Either a file-like object or a ldif.LDIFWriter instance used for output
   """
 
-  def __init__(self,l,writer_obj,headerStr='',footerStr=''):
+  def __init__(
+    self,
+    l: ldap.ldapobject.LDAPObject,
+    writer_obj: TextIO | ldif.LDIFWriter,
+    headerStr: str = '',
+    footerStr: str = '',
+  ) -> None:
     if isinstance(writer_obj,ldif.LDIFWriter):
       self._ldif_writer = writer_obj
     else:
       self._ldif_writer = ldif.LDIFWriter(writer_obj)
     FileWriter.__init__(self,l,self._ldif_writer._output_file,headerStr,footerStr)
 
-  def _processSingleResult(self,resultType,resultItem):
+  def _processSingleResult(
+    self,
+    resultType: int,
+    resultItem: LDAPSearchResult,
+  ) -> None:
     if resultType in ENTRY_RESULT_TYPES:
       # Search continuations are ignored
       dn,entry = resultItem

Lib/ldap/cidict.py
@@ -5,57 +5,72 @@ names of variable case.
 
 See https://www.python-ldap.org/ for details.
 """
+from __future__ import annotations
 import warnings
 
 from collections.abc import MutableMapping
 from ldap.pkginfo import __version__
 
+from typing import (
+    Any,
+    Iterator,
+    Mapping,
+    MutableMapping as MutableMappingType,
+    TYPE_CHECKING,
+    TypeVar,
+)
 
-class cidict(MutableMapping):
+T = TypeVar('T', bound=Any)
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+
+class cidict(MutableMappingType[str, T]):
     """
     Case-insensitive but case-respecting dictionary.
     """
     __slots__ = ('_keys', '_data')
 
-    def __init__(self, default=None):
-        self._keys = {}
-        self._data = {}
+    def __init__(self, default: Mapping[str, T] | None = None) -> None:
+        self._keys: dict[str, str] = {}
+        self._data: dict[str, T] = {}
         if default:
             self.update(default)
 
     # MutableMapping abstract methods
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> T:
         return self._data[key.lower()]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: T) -> None:
         lower_key = key.lower()
         self._keys[lower_key] = key
         self._data[lower_key] = value
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: str) -> None:
         lower_key = key.lower()
         del self._keys[lower_key]
         del self._data[lower_key]
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         return iter(self._keys.values())
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._keys)
 
     # Specializations for performance
 
-    def __contains__(self, key):
+    def __contains__(self, key: object) -> bool:
         if not isinstance(key, str):
             return False
         return key.lower() in self._keys
 
-    def clear(self):
+    def clear(self) -> None:
         self._keys.clear()
         self._data.clear()
 
-    def copy(self):
+    def copy(self) -> Self:
         inst = self.__class__.__new__(self.__class__)
         inst._data = self._data.copy()
         inst._keys = self._keys.copy()
@@ -65,12 +80,12 @@ class cidict(MutableMapping):
 
     # Backwards compatibility
 
-    def has_key(self, key):
+    def has_key(self, key: str) -> bool:
         """Compatibility with python-ldap 2.x"""
         return key in self
 
     @property
-    def data(self):
+    def data(self) -> dict[str, T]:
         """Compatibility with older IterableUserDict-based implementation"""
         warnings.warn(
             'ldap.cidict.cidict.data is an internal attribute; it may be ' +
@@ -81,7 +96,7 @@ class cidict(MutableMapping):
         return self._data
 
 
-def strlist_minus(a,b):
+def strlist_minus(a: list[str], b: list[str]) -> list[str]:
   """
   Return list of all items in a which are not in b (a - b).
   a,b are supposed to be lists of case-insensitive strings.
@@ -91,7 +106,7 @@ def strlist_minus(a,b):
     category=DeprecationWarning,
     stacklevel=2,
   )
-  temp = cidict()
+  temp: cidict[str] = cidict()
   for elt in b:
     temp[elt] = elt
   result = [
@@ -102,7 +117,7 @@ def strlist_minus(a,b):
   return result
 
 
-def strlist_intersection(a,b):
+def strlist_intersection(a: list[str], b: list[str]) -> list[str]:
   """
   Return intersection of two lists of case-insensitive strings a,b.
   """
@@ -111,7 +126,7 @@ def strlist_intersection(a,b):
     category=DeprecationWarning,
     stacklevel=2,
   )
-  temp = cidict()
+  temp: cidict[str] = cidict()
   for elt in a:
     temp[elt] = elt
   result = [
@@ -122,7 +137,7 @@ def strlist_intersection(a,b):
   return result
 
 
-def strlist_union(a,b):
+def strlist_union(a: list[str], b: list[str]) -> list[str]:
   """
   Return union of two lists of case-insensitive strings a,b.
   """
@@ -131,7 +146,7 @@ def strlist_union(a,b):
     category=DeprecationWarning,
     stacklevel=2,
   )
-  temp = cidict()
+  temp: cidict[str] = cidict()
   for elt in a:
     temp[elt] = elt
   for elt in b:

commit cd4cf982960ada123c40d5e2ece4cf4f27f971b1

fix(ldap.extop): correct RefreshRequest entryName type
Change the type of `RefreshRequest.entryName` from `str` to `bytes`.

`entryName` is passed to `pyasn1_modules.rfc2251.LDAPDN`, which is based
on `pyasn1.type.univ.OctetString` and therefore represents the value as
a byte string.

Passing a Unicode string to `OctetString` causes pyasn1 to encode it as
ISO-8859-1. LDAP distinguished names use UTF-8 encoding.  Requiring
`bytes` avoids implicitly encoding LDAP DNs with the wrong character
encoding and makes the expected representation explicit.

Issue: #368
Issue: #647
Fixes: a4110a35

Lib/ldap/extop/dds.py
@@ -39,7 +39,7 @@ class RefreshRequest(ExtendedRequest):
   def __init__(
     self,
     requestName: str | None = None,
-    entryName: str | None = None,
+    entryName: bytes | None = None,
     requestTtl: int | None = None
   ) -> None:
     super().__init__(requestName or self.requestName, b'')